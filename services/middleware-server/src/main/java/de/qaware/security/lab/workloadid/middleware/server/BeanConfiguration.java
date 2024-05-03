4670a8d796c1aa2c9ae95f44ea6437f806cff884
Christian Fritz <christian.fritz@qaware.de>
2024-05-03 18:41:09 +0200

    Add wirelog logging

--- services/middleware-server/src/main/java/de/qaware/security/lab/workloadid/middleware/server/BeanConfiguration.java
+++ services/middleware-server/src/main/java/de/qaware/security/lab/workloadid/middleware/server/BeanConfiguration.java
@@ -1,19 +1,30 @@
 package de.qaware.security.lab.workloadid.middleware.server;
 
+import io.netty.handler.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 @Configuration
 public class BeanConfiguration {
 
     @Bean
     public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        HttpClient httpClient = HttpClient.create()
+            .wiretap("reactor.netty.http.client.HttpClient",
+                LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL);
+
         ServletOAuth2AuthorizedClientExchangeFilterFunction filter =
             new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        filter.setDefaultOAuth2AuthorizedClient(true);
+
         return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
             .apply(filter.oauth2Configuration())
             .build();
     }
